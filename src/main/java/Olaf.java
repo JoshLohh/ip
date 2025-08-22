33009825e3472f6441dcdf9668e43014a5b95f9d
Josh Loh <joshloh03@gmail.com>
2025-08-22 22:47:11 +0800

    Rename Duke.java to Olaf.java and update chatbot name, greeting and exit.

---
+++ src/main/java/Olaf.java
@@ -0,0 +1,10 @@
+public class Olaf {
+    public static void main(String[] args) {
+        System.out.println("------------------------");
+        System.out.println("Heyyos! I'm Olaf! Your personal assistant:)");
+        System.out.println("What can I do for you in this beautiful day?");
+        System.out.println("------------------------");
+        System.out.println("Goodbye! Hope to see you again soon!");
+        System.out.println("------------------------");
+    }
+}
