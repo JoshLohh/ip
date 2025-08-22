073b891c6da8b1c9dcb3a9b898c8d14c17cb400b
Josh Loh <joshloh03@gmail.com>
2025-08-22 23:58:11 +0800

    Add support for Add, List.

--- src/main/java/Olaf.java
+++ src/main/java/Olaf.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Olaf {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        TaskList tasklist = new TaskList();
 
         System.out.println("  -----------------------------------------------------------------");
         System.out.println("  Heyyos! I'm Olaf! Your personal assistant:)");
@@ -12,14 +13,13 @@ public class Olaf {
         //Loop to check for user input and echo
         while(true) {
             String input = sc.nextLine();
-            if (input.equals("bye")){
+            if (input.equals("list")){
+                tasklist.listTasks();
+            } else if (input.equals("bye")){
                 break;
+            } else {
+                tasklist.addTask(input);
             }
-
-            System.out.println("  -----------------------------------------------------------------");
-            System.out.println("  " + input);
-            System.out.println("  -----------------------------------------------------------------");
-
         }
 
         System.out.println("  -----------------------------------------------------------------");
