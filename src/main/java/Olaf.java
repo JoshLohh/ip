b1dabf1d22d10bd6f4e5d8e3c4c9e5b07d840821
Josh Loh <joshloh03@gmail.com>
2025-08-22 23:24:10 +0800

    Add support for echo and ending when user inputs bye.

--- src/main/java/Olaf.java
+++ src/main/java/Olaf.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Olaf {
     public static void main(String[] args) {
-        System.out.println("------------------------");
-        System.out.println("Heyyos! I'm Olaf! Your personal assistant:)");
-        System.out.println("What can I do for you in this beautiful day?");
-        System.out.println("------------------------");
-        System.out.println("Goodbye! Hope to see you again soon!");
-        System.out.println("------------------------");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("  -----------------------------------------------------------------");
+        System.out.println("  Heyyos! I'm Olaf! Your personal assistant:)");
+        System.out.println("  What can I do for you in this beautiful day?");
+        System.out.println("  -----------------------------------------------------------------");
+
+        //Loop to check for user input and echo
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")){
+                break;
+            }
+
+            System.out.println("  -----------------------------------------------------------------");
+            System.out.println("  " + input);
+            System.out.println("  -----------------------------------------------------------------");
+
+        }
+
+        System.out.println("  -----------------------------------------------------------------");
+        System.out.println("  Goodbye! Hope to see you again soon!");
+        System.out.println("  -----------------------------------------------------------------");
     }
 }
