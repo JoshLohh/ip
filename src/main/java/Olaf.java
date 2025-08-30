64afe2d4096b127f9718b4bda2235160ffe3686d
Josh Loh <joshloh03@gmail.com>
2025-08-31 02:23:11 +0800

    Add Parser class, and Parser support for Olaf

--- src/main/java/Olaf.java
+++ src/main/java/Olaf.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeParseException;
 import java.time.format.DateTimeFormatter;
 
 
-public class Olaf {
+/*public class Olaf {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Storage storage = new Storage("./data/olaf.txt");
@@ -155,4 +155,42 @@ public class Olaf {
         System.out.println("  Goodbye! Hope to see you again soon!");
         System.out.println("  -----------------------------------------------------------------");
     }
+}*/
+
+public class Olaf {
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    public Olaf(String filePath) {
+        this.ui = new Ui();
+        this.storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(this.storage.load(), this.storage);
+        } catch (Exception e) {
+            ui.showError("Failed to load data. Starting with an empty list.");
+            tasks = new TaskList(new ArrayList<>(), this.storage);
+        }
+    }
+
+    public void run() {
+        ui.showWelcome();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            try {
+                boolean isExit = Parser.parse(input, tasks, ui);
+                if (isExit) {
+                    break;
+                }
+            } catch (OlafException e) {
+                ui.showError(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Olaf("./data/Olaf.txt");
+    }
 }
+
