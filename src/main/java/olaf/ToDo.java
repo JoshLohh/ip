3cfea78aa0502360e19499898eebf43b94d26775
Josh Loh <joshloh03@gmail.com>
2025-09-01 19:54:03 +0800

    Add JavaDoc comments for task classes

--- src/main/java/olaf/ToDo.java
+++ src/main/java/olaf/ToDo.java
@@ -1,7 +1,16 @@
 package olaf;
 
+/**
+ * Represents a ToDo task which has a description and can be marked done or undone.
+ * This class extends the base Tasl class with a type TODO.
+ */
 public class ToDo extends Task {
 
+    /**
+     * Constructs a new ToDo task with the specified description
+     *
+     * @param description The description of the ToDo task.
+     */
     public ToDo(String description) {
         super(description, TaskType.TODO);
     }
